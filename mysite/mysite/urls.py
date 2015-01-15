1851264cbcd09d927516e3cd30911ecaf9142e1b
DragonTim <1489041983@qq.com>
2015-01-15 14:22:06 +0800

    new test text

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -35,6 +35,7 @@ urlpatterns = patterns('',
         },
         name='logout'),
     #²âÊÔÏÂ
+    # test text
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
